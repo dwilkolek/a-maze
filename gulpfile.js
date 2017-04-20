663abe97e2279e9bc9b9e6f8ee1dc69ee24e6b2b
Damian Wilkołek <dwilkolek@protonmail.com>
2017-04-20 03:37:31 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp');
+var gulpCopy = require('gulp-copy');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
+var sourcemaps = require('gulp-sourcemaps');
+var clean = require('gulp-clean');
+
+var typings = [
+  'node_modules/phaser/typescript/p2.d.ts',
+  'node_modules/phaser/typescript/phaser.d.ts',
+  'node_modules/phaser/typescript/pixi.d.ts'
+]
+
+var libs = [
+  'node_modules/phaser/build/phaser.min.js'
+]
+
+var assets = [
+  'src/assets/**/*'
+]
+
+var destinationTypings = 'typings';
+var destinationLibs = 'dist/libs';
+var desinationAssets = 'dist/assets';
+
+gulp.task('clean', function () {
+  return gulp.src(['dist', 'typings'], { read: false })
+    .pipe(clean());
+});
+
+gulp.task('typings', function () {
+  gulp
+    .src(typings)
+    // .pipe(gulpCopy(destination, options))
+    // .pipe(otherGulpFunction())
+    .pipe(gulp.dest(destinationTypings));
+})
+
+
+gulp.task('clean-assets', function () {
+  return gulp.src(desinationAssets, { read: false })
+    .pipe(clean());
+});
+
+gulp.task('assets', function () {
+
+  gulp
+    .src(assets)
+    .pipe(gulp.dest(desinationAssets));
+})
+
+gulp.task('libs', function () {
+  gulp
+    .src(libs)
+    .pipe(gulp.dest(destinationLibs));
+})
+
+var tsProject = ts.createProject({
+  declaration: true,
+  out: 'client/app.js'
+});
+
+gulp.task('scripts-client', function () {
+  var tsResult = gulp.src([].concat(typings).concat(['src/client/**/*.ts']))
+    .pipe(sourcemaps.init())
+    .pipe(tsProject());
+
+  return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
+    tsResult.dts.pipe(gulp.dest('dist/definitions')),
+    tsResult.js.pipe(gulp.dest('dist/'))
+  ])
+    .pipe(sourcemaps.write());
+});
+
+gulp.task('js', function () {
+  gulp
+    .src('src/*.js')
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('client', ['typings', 'libs', 'assets', 'scripts-client', 'js'], function () {
+  gulp.watch('src/**/*.ts', ['scripts-client']);
+  gulp.watch('src/assets/**/*', ['clean-assets', 'assets']);
+  gulp.watch('src/*.js', ['js']);
+});
---
+++ src/client.js
@@ -0,0 +1,30 @@
+const express = require('express');
+var path = require('path');
+
+const app = express();
+const port = 3000
+
+app.get('/', (request, response) => {
+  response.sendFile('assets/client.html', { root: path.join(__dirname, '') })
+})
+app.get('/app.js', (request, response) => {
+  response.sendFile('client/app.js', { root: path.join(__dirname, '') })
+})
+app.use('/libs', express.static(path.join(__dirname + '/libs')));
+app.use('/assets', express.static(path.join(__dirname + '/assets')));
+
+// app.use('assets/map', express.static('./assets/map'));
+
+app.use((err, request, response, next) => {
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
